35c634a49ef78f0c972186c5999c1a4826acab0b
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-06 22:06:56 -0300

    Adicionando getters setters e contructor funcionario

--- app/model/funcionario.ts
+++ app/model/funcionario.ts
@@ -7,9 +7,50 @@ export class Funcionario extends Pessoa implements IUsuario{
     protected readonly _nome: string;
     protected _telefone: string;
     private _salario: number;
-    public cargos: Cargo[];
+    private _cargos: Cargo[] = [];
+
+    constructor(cpfFuncionario: string, nomeFuncionario: string, telefoneFuncionario: string, salarioFuncionario: number) {
+        super();
+        this._cpf = cpfFuncionario;
+        this._nome = nomeFuncionario;
+        this._telefone = telefoneFuncionario;
+        this._salario = salarioFuncionario;
+    }
 
     autenticar(): boolean {
         return true;
+    }
+
+    public get cpf() {
+        return this._cpf;
+    }
+
+    public get nome() {
+        return this._nome;
+    }
+
+    public get telefone() {
+        return this._telefone;
+    }
+
+
+    public get salario() {
+        return this._salario;
+    }
+
+    public get cargos() {
+        return  this._cargos;
+    }
+
+    public set telefone(novo: string) {
+        this._telefone = novo;
+    }
+
+    public set salario(novo: number) {
+        this._salario = novo;
+    }
+
+    public set cargo(novo: Cargo) {
+        this._cargos.push(novo);
     }
 }
