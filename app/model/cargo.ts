56c0e6b53147e597461bc55e0b84cc61e8a0df24
Thales Fossalussa <thalesvitor2001@gmail.com>
2023-01-06 22:06:36 -0300

    Adicionando getters setters e contructor Cargo

--- app/model/cargo.ts
+++ app/model/cargo.ts
@@ -1,6 +1,33 @@
 import { Funcionario } from "./funcionario.js";
 
 export class Cargo {
-    public nome: string;
-    public funcionarios: Funcionario[];
+    private readonly _nome: string;
+    private _funcionarios: Funcionario[];
+
+    constructor (novoNome: string, novoFuncionario: Funcionario) {
+        this._nome = novoNome;
+        this._funcionarios = [];
+        this._funcionarios.push(novoFuncionario);
+    }
+
+    public get nome() {
+        return this._nome;
+    }
+
+    public get funcionarios() {
+        return this._funcionarios;
+    }
+
+    public adicionarFuncionario(novoFuncionario: Funcionario): void {
+        this._funcionarios.push(novoFuncionario);
+    }
+
+    public removerFuncionario(antigoFuncionario: Funcionario): void{
+        for(let i = 0; i < this._funcionarios.length; i++) {
+            if (antigoFuncionario.cpf == this._funcionarios[i].cpf) {
+                this._funcionarios = this._funcionarios.splice(i,1);
+                break;
+            }
+        }
+    }
 }
